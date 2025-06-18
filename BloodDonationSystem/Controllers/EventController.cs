7c731552377bf87d0350ae8f69342083a030ed34
urjellyfish <annaphan0511@gmail.com>
2025-06-18 20:36:46 +0700

    refactor(api) : refactor api convention

--- BloodDonationSystem/Controllers/EventController.cs
+++ BloodDonationSystem/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using Application.DTO.EventsDTO;
+﻿using Application.DTO;
+using Application.DTO.EventsDTO;
 using Application.Service.Events;
 using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -12,45 +13,63 @@ namespace BloodDonationSystem.Controllers
     public class EventController(IEventService _eventService) : ControllerBase
     {
         [Authorize(Roles = "Staff")]
-        [HttpPost("add-event")]
+        [HttpPost]
         public async Task<IActionResult> AddEvent([FromBody] NormalEventDTO eventRequest)
         {
             if (eventRequest == null)
             {
-                return BadRequest("Event request cannot be null.");
+                return BadRequest(new ApiResponse
+                {
+                    Success = false,
+                    Message = "Event request cannot be null"
+                });
             }
             var createdEvent = await _eventService.AddEventAsync(eventRequest);
             if (createdEvent == null)
             {
-                return BadRequest("Failed to create event.");
+                return BadRequest(new ApiResponse
+                {
+                    Success = false,
+                    Message = "Failed to create event."
+                });
             }
-            return Ok(new
+            return Ok(new ApiResponse
             {
+                Success = true,
                 Message = "Event created successfully"
             });
         }
 
         [Authorize(Roles = "Staff")]
-        [HttpPost("add-urgent-event")]
+        [HttpPost("urgent-event")]
         public async Task<IActionResult> AddUrgentEvent([FromBody] UrgentEventDTO urgentEvent)
         {
             if (urgentEvent == null)
             {
-                return BadRequest("Urgent event request cannot be null.");
+                return BadRequest(new ApiResponse
+                {
+                    Success = false,
+                    Message = "Urgent request cannot be null"
+                });
             }
 
             var createdEvent = await _eventService.AddUrgentEventAsync(urgentEvent);
             if (createdEvent == null)
             {
-                return BadRequest("Failed created urgent event");
+                return BadRequest(new ApiResponse
+                {
+                    Success = false,
+                    Message = "Failed to create urgent event."
+                });
             }
-            return Ok(new
+            return Ok(new ApiResponse
             {
+                Success = true,
                 Message = "Urgent event created successfully"
             });
         }
 
-        [HttpGet("get-all")]
+        [HttpGet]
         public async Task<IActionResult> GetAllEvents([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
             var events = await _eventService.GetAllEventAsync(pageNumber, pageSize);
@@ -62,7 +81,7 @@ namespace BloodDonationSystem.Controllers
         }
 
         [Authorize(Roles = "Staff")]
-        [HttpPut("update/{eventId}")]
+        [HttpPut("{eventId}")]
         public async Task<IActionResult> UpdateEvent(int eventId, [FromBody]EventDTO updateEvent)
         {
             var eventItem = await _eventService.UpdateEventAsync(eventId, updateEvent);
@@ -78,7 +97,7 @@ namespace BloodDonationSystem.Controllers
         }
 
         [Authorize (Roles = "Staff")]
-        [HttpPut("deactive/{eventId}")]
+        [HttpPut("{eventId}/deactive")]
         public async Task<IActionResult> DeleteEvent(int eventId)
         {
             if (eventId <= 0)
@@ -86,8 +105,9 @@ namespace BloodDonationSystem.Controllers
                 return BadRequest("Invalid event ID.");
             }
             await _eventService.DeleteEventAsync(eventId);
-            return Ok(new
+            return Ok(new ApiResponse
             {
+                Success = true,
                 Message = "Event deleted successfully"
             });
         }
