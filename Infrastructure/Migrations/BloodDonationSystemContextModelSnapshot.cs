7915fd1d13d108d14da48de28f772283dbd75120
urjellyfish <annaphan0511@gmail.com>
2025-06-20 14:18:51 +0700

    fix(blood compatibility) : remove bloodTypeId foregin key

--- Infrastructure/Migrations/BloodDonationSystemContextModelSnapshot.cs
+++ Infrastructure/Migrations/BloodDonationSystemContextModelSnapshot.cs
@@ -72,12 +72,7 @@ namespace Infrastructure.Migrations
                     b.Property<int>("BloodComponent")
                         .HasColumnType("int");
 
-                    b.Property<int>("BloodTypeId")
-                        .HasColumnType("int");
-
                     b.HasKey("Id", "DonorTypeId", "RecipientTypeId");
-
-                    b.HasIndex("BloodTypeId");
 
                     b.HasIndex("DonorTypeId");
 
@@ -619,12 +614,6 @@ namespace Infrastructure.Migrations
 
             modelBuilder.Entity("Domain.Entities.BloodCompatibility", b =>
                 {
-                    b.HasOne("Domain.Entities.BloodType", "BloodType")
-                        .WithMany()
-                        .HasForeignKey("BloodTypeId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
                     b.HasOne("Domain.Entities.BloodType", "DonorType")
                         .WithMany("Donors")
                         .HasForeignKey("DonorTypeId")
@@ -636,8 +625,6 @@ namespace Infrastructure.Migrations
                         .HasForeignKey("RecipientTypeId")
                         .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
-
-                    b.Navigation("BloodType");
 
                     b.Navigation("DonorType");
 
